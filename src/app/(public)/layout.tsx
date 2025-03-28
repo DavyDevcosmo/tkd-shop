5242e69df12d3c4e78dbeae696c5e4647cf5667b
Davy cosmo <davydevcosmo@gmail.com>
2025-03-28 13:29:55 -0300

    feat: adiciona estilos e responsividade ao layout e cadastro

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,12 +1,33 @@
+import Image from "next/image";
+import Register from "./register/page";
+
+
+
 export default function PublicLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
-        <div>
-            <h1>layout public</h1>
-            {children}
-        </div>
+        <main >
+            <section className="flex  w-screen h-screen bg-white">
+                <div
+                    className="w-1/2 my-5 ml-5 rounded-2xl bg-blue-800 hidden md:block relative ">
+
+                    <Image
+                        src="/img/tkd-cadastro.png" // Substitua pelo caminho da sua imagem
+                        alt="Descrição da imagem"
+                        layout="fill"
+                        objectFit="cover"
+                        className="rounded-2xl object-cover"
+                    />
+                </div>
+
+                <div className="flex w-full h-full items-center justify-center md:w-1/2">
+
+                    <Register />
+                </div>
+            </section>
+        </main>
     )
 }
--- src/app/(public)/register/page.tsx
+++ src/app/(public)/register/page.tsx
@@ -1,32 +1,60 @@
 "use client"
 
+import InputRegister from "@/app/componets/button-register";
 import { useForm } from "react-hook-form";
 
 const Register = () => {
     return (
-        <form>
-            <div>
-                <label htmlFor="email">Email</label>
-                <input
-                    id="email"
-                    placeholder="Digite seu email"
-                    type="email" />
+        <form className="flex flex-col p-4">
+            <div className="-0">
+                <h1 className="flex justify-center font-medium font-poppins text-3xl mt-1 mb-5">Acesse a plataforma</h1>
+                <h4 className=" font-regular font-poppins text-sm mb-7">Crie sua conta!</h4>
             </div>
 
-            <div>
-                <label htmlFor="campo-senha">Senha</label>
-                <input
+            <div className="form-group">
+                <label htmlFor="email" className="btn-label-primary" >E-mail</label>
+                <InputRegister
+                    id="email"
+                    placeholder="Digite seu endereço de e-mail"
+                    type="email"
+                    className="btn-input-primary"
+
+                />
+            </div>
+
+            <div className="form-group">
+                <label htmlFor="name" className="btn-label-primary">Nome</label>
+                <InputRegister
+                    id="name"
+                    placeholder="Digite seu nome"
+                    type="name"
+                    className="btn-input-primary"
+                />
+            </div>
+
+            <div className="form-group">
+                <label htmlFor="campo-senha" className="btn-label-primary">Senha</label>
+                <InputRegister
                     id="campo-password"
                     placeholder="Digite sua senha"
-                    type="password" />
+                    type="password"
+                    className="btn-input-primary"
+                />
             </div>
 
-            <div>
-                <label htmlFor="confimPassword">Confirme sua senha</label>
-                <input
+            <div className="form-group">
+                <label htmlFor="confimPassword" className="btn-label-primary">Comfirme sua senha</label>
+                <InputRegister
                     id="confimPassword"
-                    placeholder="Digite sua senha"
-                    type="password" />
+                    placeholder="Comfirme sua senha"
+                    type="password"
+                    className="btn-input-primary"
+                />
+            </div>
+            <div>
+                <button className="
+                w-full h-12 md:h-14 rounded-full bg-[#0A1CA4] hover:bg-[#0D26DF] text-white font-poppins font-medium mt-5 mb-2 cursor-pointer transition-colors lg:mt-15"
+                >Cadastre-se</button>
             </div>
         </form>
     )
