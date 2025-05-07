a9879f68767ff3b9398512d55539aaca72610246
Davy cosmo <davydevcosmo@gmail.com>
2025-05-06 22:04:23 -0300

    Feat: criação do middleware com a proteção de rotas e configuração do jwt

---
+++ src/middleware.ts
@@ -0,0 +1,59 @@
+import { MiddlewareConfig, NextRequest, NextResponse } from "next/server"
+
+const publicRoutes = [
+  { path: '/auth/login', whenAuthenticated: 'next' },
+  { path: '/auth/register', whenAuthenticated: 'next' },
+  { path: '/dasboard', whenAuthenticated: 'redirect' },
+  { path: '/', whenAuthenticated: 'next' },
+] as const
+
+const REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE = '/auth/login'
+
+function isJwtExpired(token: string): boolean {
+  try {
+    const payloadBase64 = token.split('.')[1]
+    const payloadJson = atob(payloadBase64)
+    const payload = JSON.parse(payloadJson)
+    const now = Math.floor(Date.now() / 1000)
+
+    return payload.exp < now
+  } catch (err) {
+    return true // Se der erro ao decodificar, considera expirado/inválido
+  }
+}
+
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname
+  const publicRoute = publicRoutes.find(route => route.path === path)
+  const authToken = request.cookies.get('token')?.value
+
+  if (!authToken && publicRoute) {
+    return NextResponse.next()
+  }
+
+  if (!authToken && !publicRoute) {
+    const redirectUrl = request.nextUrl.clone()
+    redirectUrl.pathname = REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE
+    return NextResponse.redirect(redirectUrl)
+  }
+
+  if (authToken && isJwtExpired(authToken)) {
+    const response = NextResponse.redirect(new URL('/auth/login', request.url))
+    response.cookies.delete('token') // Remove cookie expirado
+    return response
+  }
+
+  if (authToken && publicRoute && publicRoute.whenAuthenticated === 'redirect') {
+    const redirectUrl = request.nextUrl.clone()
+    redirectUrl.pathname = '/dasboard'
+    return NextResponse.redirect(redirectUrl)
+  }
+
+  return NextResponse.next()
+}
+
+export const config: MiddlewareConfig = {
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+  ],
+}
