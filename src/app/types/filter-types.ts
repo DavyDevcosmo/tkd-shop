ba9830322bd1bf0af873be3774b88cf375e48df2
Davy cosmo <davydevcosmo@gmail.com>
2025-06-10 22:04:56 -0300

    Feat: emplementei o FILTER_TYPE_TO_CATEGORY

--- src/app/types/filter-types.ts
+++ src/app/types/filter-types.ts
@@ -1,7 +1,15 @@
 export enum FilterType {
-    All = 'All',
+    ALL = 'ALL',
     PROTECTION = 'PROTECTION',
     DOBOK = 'DOBOK',
     TAEKWONDOBELT = 'TAEKWONDOBELT',
     TAEKWONDO = 'TAEKWONDO',
 }
+
+export const FILTER_TYPE_TO_CATEGORY: Record<FilterType, string> = {
+    [FilterType.ALL]: "Todos",
+    [FilterType.DOBOK]: "Doboks",
+    [FilterType.PROTECTION]: "Proteções",
+    [FilterType.TAEKWONDOBELT]: "Faixas",
+    [FilterType.TAEKWONDO]: "Taekwondo" // Ajuste conforme sua categoria real
+} as const;
