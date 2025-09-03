f98828f68da4f803852b149a8949aaf593a25646
Davy cosmo <davydevcosmo@gmail.com>
2025-09-03 05:26:02 -0300

    feat: função de tratamento de email, e cripitografia de senha

--- src/app/(public)/auth/actions/registerUser.ts
+++ src/app/(public)/auth/actions/registerUser.ts
@@ -1,8 +1,13 @@
-"use server"
+"use server";
 
-import { schemaRegister } from "../validations/registerSchema"
+import bcrypt from "bcryptjs";
+
+import { schemaRegister } from "../validations/registerSchema";
+import db from "../../../../../prisma/db";
+
 
 export async function registerUser(formData: FormData) {
+ 
   const data = {
     name: formData.get("name")?.toString() || "",
     email: formData.get("email")?.toString() || "",
@@ -10,17 +15,32 @@ export async function registerUser(formData: FormData) {
     confirmPassword: formData.get("confirmPassword")?.toString() || "",
   };
 
+  
   const parsed = schemaRegister.safeParse(data);
-
   if (!parsed.success) {
-    console.log("Erro de validação:", parsed.error.flatten().fieldErrors);
     return { success: false, errors: parsed.error.flatten().fieldErrors };
   }
 
-  await new Promise((resolve) => setTimeout(resolve, 1500));
+ 
+  const existingUser = await db.user.findUnique({
+    where: { email: parsed.data.email },
+  });
+  if (existingUser) {
+    return { success: false, errors: { email: ["E-mail já cadastrado"] } };
+  }
 
-  console.log("Usuário criado:", parsed.data);
 
+  const hashedPassword = await bcrypt.hash(parsed.data.password, 10);
 
-  return { success: true, errors: {} };
+ 
+  const user = await db.user.create({
+    data: {
+      name: parsed.data.name,
+      email: parsed.data.email,
+      
+    },
+  });
+
+return { success: true, errors: {} };
+
 }
