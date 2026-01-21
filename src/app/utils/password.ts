8ca03f771ed8fd37bb65d1dec3646123821fb92b
Davy cosmo <davydevcosmo@gmail.com>
2026-01-21 19:08:38 -0300

    feat: adicionar funções para hash e verificação de senhas utilizando bcrypt

---
+++ src/app/utils/password.ts
@@ -0,0 +1,14 @@
+// utils/password.ts
+import bcrypt from "bcryptjs";
+
+
+export async function saltAndHashPassword(password: string) {
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  return hashedPassword;
+}
+
+
+export async function verifyPassword(password: string, hashedPassword: string) {
+  return await bcrypt.compare(password, hashedPassword);
+}
